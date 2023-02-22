f2498a921a3766ad2535c195c1881e0803b9ca11
Mayur Jain <maxcharge788769@gmail.com>
2023-02-22 18:06:17 +0530

    [UPDATE] Folder structure

---
+++ mayur/Hangman/main.rb
@@ -0,0 +1,17 @@
+WORDS = %w[kylas amura selldo beyondwalls].freeze
+$word = WORDS.sample
+$chances = 6
+def hangman(word, chances)
+  chances = 6
+  letters = ''
+  word.length.times { print '_' }
+  p
+  while (letters.length < word.length) && (chances > 0)
+    print "Guess it Chances:#{chances} :"
+    letter = gets.chomp
+    chances -= 1 if word.count(letter) <= 0 || letters.count(letter) > 0
+    word.count(letter).times { letters += letter }
+    p word.gsub(/[^#{letters}]/, '_')  if word.count(letter) > 0
+  end
+  letters.length == word.length ? 'You won the game' : 'You lose the game'
+end
