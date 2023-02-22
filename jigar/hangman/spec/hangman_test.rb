f4d1d2a9af63c0ff3c21ddf39de0bfb9673d5acf
Makwana-Jigar <124685204+Makwana-Jigar@users.noreply.github.com>
2023-02-23 00:33:39 +0530

    updated one expect for one it

--- jigar/hangman/spec/hangman_test.rb
+++ jigar/hangman/spec/hangman_test.rb
@@ -1,9 +1,8 @@
-require '/home/dell/Documents/ruby/hangman/hangman'
+require '/home/dell/ruby_Kylas/jigar/hangman/hangman'
 
 describe "Hangman Game: " do
-  before do
-    $secret_word = "aaabbbcccc"
-    $word = $secret_word.split("")
+  before(:each) do
+    $word = SECRET_WORD.clone
     $guess_word = " _" * $word.length
     $max_chances = 6
     $correct_guesses = 0
@@ -11,64 +10,74 @@ describe "Hangman Game: " do
   describe "Testing of check_the_guess(letter) => " do
     context "when user guessed the correct letter" do
       it "then it should return the updated state of guess word" do
-        expect{check_the_guess("a")}.to output("\nGood guess! You have guessed the correct letter 'a'.\n").to_stdout
+        check_the_guess("a")
         expect($guess_word).to eq(" a _ _ _ _ _ _ _ _ _")
       end
 
       it "then it should increase the correct guesses" do
-        expect{check_the_guess("a")}.to output("\nGood guess! You have guessed the correct letter 'a'.\n").to_stdout
+        check_the_guess("a")
         expect($correct_guesses).to eq(1)
       end
 
       it "then it should not reduce the maximum chances" do
-        expect{check_the_guess("a")}.to output("\nGood guess! You have guessed the correct letter 'a'.\n").to_stdout
+        check_the_guess("a")
         expect($max_chances).to eq(6)
       end
     end
 
     context "when user guessed the wrong letter" do
       it "then it should not change the state of guess word" do
-        expect{check_the_guess("o")}.to output("\nSorry, You have guessed the wrong letter 'o'.\n").to_stdout
+        check_the_guess("x")
         expect($guess_word).to eq(" _ _ _ _ _ _ _ _ _ _")
       end
 
       it "then it should not increase the correct guesses" do
-        expect{check_the_guess("o")}.to output("\nSorry, You have guessed the wrong letter 'o'.\n").to_stdout
+        check_the_guess("x")
         expect($correct_guesses).to eq(0)
       end
 
       it "then it should reduce the maximum chances" do
-        expect{check_the_guess("o")}.to output("\nSorry, You have guessed the wrong letter 'o'.\n").to_stdout
+        check_the_guess("x")
         expect($max_chances).to eq(5)
       end
     end
   end
 
-  describe "Testing of game_is_over_or_not? and win? => " do
+  describe "Testing of game_is_over_or_not? => " do
     
     context "when the game is not over yet" do
-      it "then game_is_over_or_not? should return false" do
+      it "then it should return false" do
         expect(game_is_over_or_not?).to be false
       end
     end
 
-    context "when the game is over and has won by player" do
-      it "then game_is_over_or_not? should return true and win? should print the winning message" do
-        
-        $secret_word.chars.each {|letter| expect{check_the_guess(letter)}.to output("\nGood guess! You have guessed the correct letter '#{letter}'.\n").to_stdout}
-
+    context "when the game is over because the player has won" do
+      it "then it should return true" do
+        SECRET_WORD.chars.each {|letter| check_the_guess(letter)}
         expect(game_is_over_or_not?).to be true
-        expect{ win? }.to output("\nCongratulations! You guessed the correct word '#{$secret_word}'!\n").to_stdout
       end
     end
 
-    context "when the game is over and has lost by player" do
-      it "then game_is_over_or_not? should return true and win? should print the losing message" do
-        
-        $max_chances.times {expect{check_the_guess("x")}.to output("\nSorry, You have guessed the wrong letter 'x'.\n").to_stdout}
-
+    context "when the game is over because the player has lost" do
+      it "then it should return true" do
+        $max_chances.times {check_the_guess("x")}
         expect(game_is_over_or_not?).to be true
-        expect{ win? }.to output("\nSorry, you ran out of guesses. The word was '#{$secret_word}'.\n").to_stdout
+      end
+    end
+  end
+
+  describe "Testing win? => " do
+    context "when the player has won" do
+      it "then it should print the winning message" do
+        SECRET_WORD.chars.each {|letter| check_the_guess(letter)}
+        expect{ win? }.to output("\nCongratulations! You guessed the correct word '#{SECRET_WORD}'!\n").to_stdout
+      end
+    end
+
+    context "when the player has lost" do
+      it "then it should print the losing message" do
+        $max_chances.times {check_the_guess("x")}
+        expect{ win? }.to output("\nSorry, you ran out of guesses. The word was '#{SECRET_WORD}'.\n").to_stdout
       end
     end
   end
