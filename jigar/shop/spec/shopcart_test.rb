cb83fe2d20acc4162e8e33f0783d5cbe4cd754e1
Makwana-Jigar <124685204+Makwana-Jigar@users.noreply.github.com>
2023-02-23 15:09:00 +0530

    Updated one it contains only one expect

--- jigar/shop/spec/shopcart_test.rb
+++ jigar/shop/spec/shopcart_test.rb
@@ -1,4 +1,4 @@
-require '/home/dell/Documents/ruby/shop/shopcart'
+require '/home/dell/ruby_Kylas/jigar/shop/shopcart'
 
 describe "Shop_cart" do
     before do
@@ -9,11 +9,8 @@ describe "Shop_cart" do
     describe "Testing of add_product_to_stock => " do
         it "it should add the new product in the stock" do
             expect_any_instance_of(Kernel).to receive(:gets).and_return("Oneplus buds\n", "12.4 mm driver size\n", "1500.00\n", "20\n")
-            expect{add_product_to_stock}.to output("\nEnter the name of the product: \nEnter the description of the product: \nEnter the price of the product: \nEnter the quantity of the product: \nProduct 'Oneplus buds' has successfully added").to_stdout
-            expect($stock.size).to eq(1)
-            expect($stock["Oneplus buds"][:description]).to eq("12.4 mm driver size")
-            expect($stock["Oneplus buds"][:price]).to eq(1500.0)
-            expect($stock["Oneplus buds"][:quantity]).to eq(20)
+            add_product_to_stock
+            expect($stock).to eq({"Oneplus buds" => {:description => "12.4 mm driver size", :price => 1500.0, :quantity => 20}})
         end
     end
 
@@ -40,20 +37,30 @@ describe "Shop_cart" do
 
         context "when there's enough quantity of product in the stock" do
             context "and the product is new to the cart" do
-                it "then it should add a product in the cart" do
+                before(:each) do
                     $stock = {
                         "SONY TV" => {:description => "43 inch LED", :price => 45000.0, :quantity => 50},
                     }
-                    $cart = {}    
-                    expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
-                    expect{add_product_to_cart}.to output("\nEnter the name of the product to add to cart: \nEnter the quantity to add to cart: \n\nProduct 'SONY TV' has successfully added to cart\n").to_stdout 
-                    expect($cart.size).to eq(1)
-                    expect($cart["SONY TV"][:quantity]).to eq(10)
-                    expect($stock["SONY TV"][:quantity]).to eq(40)
+                    $cart = {}  
                 end
+                it "then it should add a product in the cart and cart size increases by 1" do  
+                    expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
+                    add_product_to_cart
+                    expect($cart.size).to eq(1)
+                end
+                it "then it should add a product in the cart and product quantity should be 10 in cart" do
+                    expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
+                    add_product_to_cart
+                    expect($cart["SONY TV"][:quantity]).to eq(10)
+                end
+                it "then it should add a product in the cart and product quantity should be reduced by 10 in stock" do
+                    expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
+                    add_product_to_cart
+                    expect($stock["SONY TV"][:quantity]).to eq(40)
+                end                
             end
             context "and the product has already added in the cart" do
-                it "then it should update a product quantity in the cart" do
+                before(:each) do
                     $stock = {
                         "SONY TV" => {:description => "43 inch LED", :price => 45000.0, :quantity => 40},
                     }
@@ -64,33 +71,65 @@ describe "Shop_cart" do
                             :quantity => 10
                         }
                     }
+                end
+                it "then it should update a product quantity in the cart and cart size should not be changed" do
                     expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
-                    expect{add_product_to_cart}.to output("\nEnter the name of the product to add to cart: \nEnter the quantity to add to cart: \n\nProduct 'SONY TV' has successfully added to cart\n").to_stdout 
+                    add_product_to_cart
                     expect($cart.size).to eq(1)
+                end
+                it "then it should update a product quantity in the cart and product quantity should increase by 10 in cart" do
+                    expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
+                    add_product_to_cart
                     expect($cart["SONY TV"][:quantity]).to eq(20)
+                end
+                it "then it should update a product quantity in the cart and product quantity should reduce by 10 in stock" do
+                    expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "10\n")
+                    add_product_to_cart
                     expect($stock["SONY TV"][:quantity]).to eq(30)
                 end
             end
         end
         context "when there's not enough quantity of product in the stock" do
-            it "then it should give an error message" do
+            before(:each) do
                 $stock = {
                     "SONY TV" => {:description => "43 inch LED", :price => 45000.0, :quantity => 50},
                 }
+                $cart = {}
+            end
+            it "then it should give an error message" do
                 expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "60\n")
                 expect{ add_product_to_cart }.to output("\nEnter the name of the product to add to cart: \nEnter the quantity to add to cart: \n\nSorry! We don't have enough stocks of Product 'SONY TV'\n").to_stdout
+            end
+            it "then it should not add the product in cart and cart size should not changed" do
+                expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "60\n")
+                add_product_to_cart
                 expect($cart.size).to eq(0)
+            end
+            it "then it should not add the product in cart and product quantity should not changed in stock" do
+                expect_any_instance_of(Kernel).to receive(:gets).and_return("SONY TV\n", "60\n")
+                add_product_to_cart
                 expect($stock["SONY TV"][:quantity]).to eq(50)
             end
         end
         context "when the product is not found in the stock" do
-            it "then it should give an error message" do
+            before(:each) do
                 $stock = {
                     "SONY TV" => {:description => "43 inch LED", :price => 45000.0, :quantity => 50},
                 }
+                $cart = {}
+            end
+            it "then it should give an error message" do
                 expect_any_instance_of(Kernel).to receive(:gets).and_return("NOISE\n")
                 expect{ add_product_to_cart }.to output("\nEnter the name of the product to add to cart: \n\nWe don't have Product 'NOISE' in our stocks\n").to_stdout
+            end
+            it "then it should not add product in cart" do
+                expect_any_instance_of(Kernel).to receive(:gets).and_return("NOISE\n")
+                add_product_to_cart
                 expect($cart.size).to eq(0)
+            end
+            it "then it should not modify the stock" do
+                expect_any_instance_of(Kernel).to receive(:gets).and_return("NOISE\n")
+                add_product_to_cart
                 expect($stock.size).to eq(1)
             end
         end
@@ -115,8 +154,4 @@ describe "Shop_cart" do
         end
     end
 end
-
-# Hello THere
-
-
     
